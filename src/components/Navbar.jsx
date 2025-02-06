5fda3cffa4461532ce22acce0a02f379bdcef19a
Garry Caber <garrypedrosa.31@gmail.com>
2025-02-06 11:00:36 +0800

    Final Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,26 +4,33 @@ import { useRouter } from 'next/router';
 import Image from 'next/image'
 
 function Navbar(props) {
-  const navbar = useRouter();
+  const navbarRoute = useRouter();
+
+  const navigateTo = (path) => {
+    navbarRoute.push(path);
+  }
   return (
 
-    <div className="w-full bg-[#ffffff57] border-b border-b-white h-max text-white pt-[25px] px-9 pb-[20px] fixed
+    <div className="w-full bg-[#ffffff57] h-max text-white pt-[25px] px-9 pb-[20px] fixed
       flex gap-[70px] item-center justify-center">
       <h1 className="text-[57px] font-semibold tracking-tight leading-[64px]">BLOGFIX</h1>
       <ul className="flex flex-1 min-w-[500px] px-[38px] gap-[25px] justify-end">
-        <li className="border-b-[3px] border-[#050D1B]
-          p-[10px] flex items-center text-[28px] font-semibold text-white">Home</li>
-        <li className="border-b-[3px] border-[#D8C4B6BF]
+        <li className="border-b-[3px] border-[#050D1B] cursor-pointer
+          p-[10px] flex items-center text-[28px] font-semibold text-white"
+          onClick={() => navigateTo("/")}>Home</li>
+        <li className="border-b-[3px] border-[#D8C4B6BF] cursor-pointer
           p-[10px] flex items-center text-[28px] font-semibold text-[#FFFFFF80]">Blog</li>
-        <li className="border-b-[3px] border-[#D8C4B6BF]
-          p-[10px] flex items-center text-[28px] font-semibold text-[#FFFFFF80]">Create</li>
+        <li className="border-b-[3px] border-[#D8C4B6BF] cursor-pointer
+          p-[10px] flex items-center text-[28px] font-semibold text-[#FFFFFF80]"
+          onClick={() => navigateTo("/create")}>Create</li>
       </ul>
         <Image
           src="/Login.png"
           width={50}
           height={50}
-          className='h-[50px]'
+          className='h-[50px] cursor-pointer'
           alt="Login Button"
+          onClick={() => navigateTo("/login")}
         />
     </div>
   );
