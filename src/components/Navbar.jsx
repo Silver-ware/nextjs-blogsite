64e7be25be6e131d1212efc222cb00fd351f21f0
Garry Caber <garrypedrosa.31@gmail.com>
2025-02-06 13:14:01 +0800

    Add hover effects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,16 +11,16 @@ function Navbar(props) {
   }
   return (
 
-    <div className="w-full bg-[#ffffff57] h-max text-white pt-[25px] px-9 pb-[20px] fixed
+    <div className="w-full bg-[#213555] bg-opacity-[49%] h-max text-white pt-[25px] px-9 pb-[20px] fixed
       flex gap-[70px] item-center justify-center">
       <h1 className="text-[57px] font-semibold tracking-tight leading-[64px]">BLOGFIX</h1>
       <ul className="flex flex-1 min-w-[500px] px-[38px] gap-[25px] justify-end">
-        <li className="border-b-[3px] border-[#050D1B] cursor-pointer
+        <li className="border-b-[3px] border-[#050D1B] cursor-pointer hover:scale-110 transition-all ease-in-out
           p-[10px] flex items-center text-[28px] font-semibold text-white"
           onClick={() => navigateTo("/")}>Home</li>
-        <li className="border-b-[3px] border-[#D8C4B6BF] cursor-pointer
+        <li className="border-b-[3px] border-[#D8C4B6BF] cursor-pointer hover:scale-110 transition-all ease-in-out
           p-[10px] flex items-center text-[28px] font-semibold text-[#FFFFFF80]">Blog</li>
-        <li className="border-b-[3px] border-[#D8C4B6BF] cursor-pointer
+        <li className="border-b-[3px] border-[#D8C4B6BF] cursor-pointer hover:scale-110 transition-all ease-in-out
           p-[10px] flex items-center text-[28px] font-semibold text-[#FFFFFF80]"
           onClick={() => navigateTo("/create")}>Create</li>
       </ul>
@@ -28,7 +28,7 @@ function Navbar(props) {
           src="/Login.png"
           width={50}
           height={50}
-          className='h-[50px] cursor-pointer'
+          className='h-[50px] cursor-pointer hover:scale-125 transition-all ease-in-out'
           alt="Login Button"
           onClick={() => navigateTo("/login")}
         />
