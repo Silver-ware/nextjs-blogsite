458f74785c3c18489d67b7632954abf00721ceae
Ianne <bulilaniannecarl@gmail.com>
2025-02-06 17:10:37 +0800

    finalized view-blog

--- src/pages/view/index.jsx
+++ src/pages/view/index.jsx
@@ -4,12 +4,12 @@ import { IoMdShare } from "react-icons/io";
 
 export default function index() {
   return (
-    <div className="bg-gradient-to-b from-white from-[80%] to-[#3E5879] w-full h-[1024px] flex flex-col">
+    <div className="bg-gradient-to-b from-white from-[80%] to-[#3E5879] w-full h-[1024px] flex flex-col relative">
         <div className="flex items-end w-full h-full bg-black bg-[url(https://images.unsplash.com/photo-1542662565-7e4b66bae529?q=80&w=1527&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-center bg-cover">
             <div className="w-full flex justify-center text-white p-[10px] gap-[10px]">
-                <div className="px-[10px] bg-white bg-opacity-[25%] py-[3px] cursor-pointer">Mountain</div>
-                <div className="px-[10px] bg-white bg-opacity-[25%] py-[3px] cursor-pointer">Scenery</div>
-                <div className="px-[10px] bg-white bg-opacity-[25%] py-[3px] cursor-pointer">Nature</div>    
+                <div className="px-[10px] bg-white bg-opacity-[25%] py-[3px] cursor-pointer hover:scale-105 scale-100 transition-all duration-200 ease-in-out">Mountain</div>
+                <div className="px-[10px] bg-white bg-opacity-[25%] py-[3px] cursor-pointer hover:scale-105 scale-100 transition-all duration-200 ease-in-out">Scenery</div>
+                <div className="px-[10px] bg-white bg-opacity-[25%] py-[3px] cursor-pointer hover:scale-105 scale-100 transition-all duration-200 ease-in-out">Nature</div>    
             </div>
         </div>  
         <div className="w-full h-full flex flex-col px-[40px] gap-[10px] py-[25px] text-[#213555]">
@@ -37,7 +37,7 @@ export default function index() {
             </div>
 
             <div  className="w-full p-[10px] flex items-center">
-                <Link href={'/'} className="text-[40px] font-semibold underline underline-offset-[18%]">View Other Blogs</Link>
+                <Link href={'/'} className="text-[40px] font-semibold hover:underline hover:underline-offset-[18%]">View Other Blogs</Link>
             </div>
         </div>
     </div>
